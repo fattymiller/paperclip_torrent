358c78815f242dca409ea8add0f7b7a25283e812
Fatty <fattymiller@gmail.com>
2014-04-04 14:44:18 +1000

    rejigged torrentable to prevent a null reference exception from being raised when only partially used

--- lib/paperclip_torrent/extensions/torrentable.rb
+++ lib/paperclip_torrent/extensions/torrentable.rb
@@ -7,15 +7,17 @@ module PaperclipTorrent
 
       has_many :torrent_files, class_name: "PaperclipTorrent::TorrentFileAttachment", as: :torrentable      
 
-      def add_torrentable_field(field_name)
+      def torrentable_fields
         @torrentable_fields ||= []
-        @torrentable_fields << field_name if !@torrentable_fields.include?(field_name)
+      end
+      def add_torrentable_field(field_name)
+        torrentable_fields << field_name if !torrentable_fields.include?(field_name)
       end
       
       private 
       
       def persist_torrent_files
-        @torrentable_fields.uniq.each { |field_name| self.respond_to?(field_name) && send(field_name).persist_torrents }
+        torrentable_fields.uniq.each { |field_name| self.respond_to?(field_name) && send(field_name).persist_torrents }
       end
     end
   end
